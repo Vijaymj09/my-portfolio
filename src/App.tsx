a9debc2bd1bd0381cb4013052ef0f65e37286e44
Vijaymj09 <karthimj09@gmail.com>
2025-04-11 12:22:21 +0530

    Add @tsparticles/slim dependency, import App.css in App component, and move body styles to Footer.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import ParticlesBackground from "./components/ParticlesBackground";
+import "./App.css";
 
 function App() {
   return (
